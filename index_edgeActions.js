73fdcfb18ad5dbb1a5e2de75e9f43e36440d66b6
MatthewMularoni <matthew.mularoni@gmail.com>
2017-04-11 19:06:30 -0500

    clone

---
+++ index_edgeActions.js
@@ -0,0 +1,62 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_homepageBTN}","click",function(sym,e){window.open("http://www.nwmissouri.edu/online/","_blank");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_homepageBTN}","mouseover",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_searchBTN}","click",function(sym,e){window.open("https://ssb.nwmissouri.edu/pls/PRODDAD/nwcrsewo.P_Showschedule","_blank");});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'chat1'
+(function(symbolName){})("chat1");
+//Edge symbol end:'chat1'
+
+//=========================================================
+
+//Edge symbol: 'chat2'
+(function(symbolName){})("chat2");
+//Edge symbol end:'chat2'
+
+//=========================================================
+
+//Edge symbol: 'Chat3'
+(function(symbolName){})("Chat3");
+//Edge symbol end:'Chat3'
+
+//=========================================================
+
+//Edge symbol: 'Chat'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",1865,function(sym,e){});
+//Edge binding end
+})("Chat");
+//Edge symbol end:'Chat'
+
+//=========================================================
+
+//Edge symbol: 'homepageBTN'
+(function(symbolName){})("homepageBTN");
+//Edge symbol end:'homepageBTN'
+
+//=========================================================
+
+//Edge symbol: 'searchBTN'
+(function(symbolName){})("searchBTN");
+//Edge symbol end:'searchBTN'
+
+//=========================================================
+
+//Edge symbol: 'endText'
+(function(symbolName){})("endText");
+//Edge symbol end:'endText'
+
+//=========================================================
+
+//Edge symbol: 'Down-level'
+(function(symbolName){})("Down-level");
+//Edge symbol end:'Down-level'
+})(jQuery,AdobeEdge,"EDGE-488472437");
